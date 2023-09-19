ba9baed7aceff2576ffc128e7282cd0f2c99180b
Marc Link <linkcla10@gmail.com>
2023-09-19 21:49:54 +0200

    [NEW] Ejercicio 2.2, he ikmplementado una pila mediante referencias

--- PilaReferencia/src/Main.java
+++ PilaReferencia/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        //
-        PilaReferencia<Integer> pila = new PilaReferencia<Integer>();
+
+        PilaReferencia<Integer> pila = new PilaReferencia<>();
         pila.push(0);
         try {
             int i = pila.pop();
